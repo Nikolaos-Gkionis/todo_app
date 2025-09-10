bfc380bea7493d1698b571fe9d29e7fcc2a1d30c
Nikolaos <95080307+Nikolaos-Gkionis@users.noreply.github.com>
2025-09-10 14:06:00 +0100

    Implement project and todo creation with UI improvements
    Added full CRUD logic for creating projects and todos, including strong parameters and temporary user assignment. Updated the projects index to link to project details, redesigned the new project and project show pages with forms and todo lists, and added flash message display to the application layout for better user feedback.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,12 +4,31 @@ class ProjectsController < ApplicationController
   end
 
   def show
+    @project = Project.find(params[:id])
+    @todos = @project.todos
+    @new_todo = @project.todos.build
   end
 
   def new
+    @project = Project.new
+    @project.user = User.first  # Temporary: assign to first user
   end
 
   def create
+    @project = Project.new(project_params)
+    @project.user = User.first  # Temporary: assign to first user
+  
+    if @project.save
+      redirect_to projects_path, notice: 'Project was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description, :cover_color)
   end
 
   def edit
--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,6 +6,22 @@ class TodosController < ApplicationController
   end
 
   def create
+    @project = Project.find(params[:project_id])
+    @todo = @project.todos.build(todo_params)
+    
+    if @todo.save
+      redirect_to @project, notice: 'Todo was successfully created.'
+    else
+      @todos = @project.todos
+      @new_todo = @todo  # Keep the failed todo to show errors
+      render 'projects/show'
+    end
+  end
+  
+  private
+  
+  def todo_params
+    params.require(:todo).permit(:title, :notes, :completed, :position)
   end
 
   def edit
