b91654a481f928b7d443237abeda6ba82d74513d
Nikolaos <95080307+Nikolaos-Gkionis@users.noreply.github.com>
2025-09-10 12:25:38 +0100

    Add projects and todos controllers, views, and routes
    Introduces ProjectsController and TodosController with standard RESTful actions, along with corresponding helper modules and basic view templates. Updates models to add associations and validations for Project, Todo, and User. Configures nested resources in routes, setting projects#index as the root path.

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,6 @@
 class Todo < ApplicationRecord
   belongs_to :project
+
+  validates :title, presence: true, length: { minimum: 1, maximum: 200 }
+  validates :project, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_many :projects, dependent: :destroy
+
+    validates :email_address, presence: true, uniqueness: true
+    validates :email_address, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
