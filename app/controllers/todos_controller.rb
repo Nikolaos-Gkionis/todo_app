b76520bbe390f4468763cfca3b9c385d05151d1d
Nikolaos <95080307+Nikolaos-Gkionis@users.noreply.github.com>
2025-09-11 09:22:07 +0100

    Refactor todos controller and update project show view
    Refactored TodosController to use before_action callbacks for setting project and todo, and improved CRUD actions for todos. Updated the project show view to support inline editing of todo completion status and improved form structure for better user experience.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,35 +1,56 @@
 class TodosController < ApplicationController
+  before_action :set_project
+  before_action :set_todo, only: [:edit, :update, :destroy]
+
   def index
+    redirect_to @project
   end
 
   def new
+    @todo = @project.todos.build
   end
 
   def create
-    @project = Project.find(params[:project_id])
     @todo = @project.todos.build(todo_params)
     
     if @todo.save
       redirect_to @project, notice: 'Todo was successfully created.'
     else
       @todos = @project.todos
-      @new_todo = @todo  # Keep the failed todo to show errors
+      @new_todo = @todo
       render 'projects/show'
     end
-  end
-  
-  private
-  
-  def todo_params
-    params.require(:todo).permit(:title, :notes, :completed, :position)
   end
 
   def edit
   end
 
   def update
+    if @todo.update(todo_params)
+      redirect_to @project, notice: 'Todo was successfully updated.'
+    else
+      @todos = @project.todos
+      @new_todo = @project.todos.build
+      render 'projects/show'
+    end
   end
 
   def destroy
+    @todo.destroy
+    redirect_to @project, notice: 'Todo was successfully deleted.'
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def set_todo
+    @todo = @project.todos.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:title, :notes, :completed, :position)
   end
 end
